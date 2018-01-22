be1a68ff341816d7c78fff0070ee13249ce66ef3
Halogen002 <Halogen002@users.noreply.github.com>
2018-01-21 18:47:53 -0600

    Remove map preload event

--- client/hooks/map_load.h
+++ client/hooks/map_load.h
@@ -4,6 +4,3 @@
 
 void add_map_load_event(event_no_args event_function, EventPriority priority = EVENT_PRIORITY_DEFAULT) noexcept;
 void remove_map_load_event(event_no_args event_function) noexcept;
-
-void add_map_preload_event(event_no_args event_function, EventPriority priority = EVENT_PRIORITY_DEFAULT) noexcept;
-void remove_map_preload_event(event_no_args event_function) noexcept;
