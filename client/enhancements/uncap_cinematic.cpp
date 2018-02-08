bd0d4b68ed03321c49c8c475aff212614354b7cf
Halogen002 <Halogen002@users.noreply.github.com>
2018-02-08 10:47:01 -0600

    Uncap cinematic does not need to modify framerate throttler anymore

--- client/enhancements/uncap_cinematic.cpp
+++ client/enhancements/uncap_cinematic.cpp
@@ -3,22 +3,21 @@
 #include "uncap_cinematic.h"
 
 ChimeraCommandError uncap_cinematic_command(size_t argc, const char **argv) noexcept {
-    static bool active = false;
+    extern bool uncap_cutscenes;
     if(argc == 1) {
         auto &uncap_cinematic_s = get_signature("uncap_cinematic_sig");
         bool new_value = bool_value(argv[0]);
-        if(new_value != active) {
+        if(new_value != uncap_cutscenes) {
             if(new_value) {
                 const short uncap_cinematic_mod[] = {0xEB, 0x04, 0xB3, 0x01, 0xEB, 0x02, 0x32, 0xDB, 0x8B, 0x2D};
                 write_code_s(uncap_cinematic_s.address(),uncap_cinematic_mod);
             }
             else {
                 uncap_cinematic_s.undo();
-                execute_chimera_command("chimera_throttle_fps 0", true);
             }
-            active = new_value;
+            uncap_cutscenes = new_value;
         }
     }
-    console_out(std::string("chimera_uncap_cinematic: ") + (active ? "true" : "false"));
+    console_out(std::string("chimera_uncap_cinematic: ") + (uncap_cutscenes ? "true" : "false"));
     return CHIMERA_COMMAND_ERROR_SUCCESS;
 }
