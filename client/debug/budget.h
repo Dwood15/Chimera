ab55f362cc484a42144c9fc7b78ca89541fc517c
Halogen002 <Halogen002@users.noreply.github.com>
2018-02-20 11:10:30 -0600

    Put player_info_command in header

--- client/debug/budget.h
+++ client/debug/budget.h
@@ -4,3 +4,6 @@
 
 /// Function for command chimera_budget
 ChimeraCommandError budget_command(size_t argc, const char **argv) noexcept;
+
+/// Function for command chimera_player_info
+ChimeraCommandError player_info_command(size_t argc, const char **argv) noexcept;
