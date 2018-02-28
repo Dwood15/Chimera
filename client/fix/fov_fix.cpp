b425f16109e5f7d1d7e3f22c7f15404b4e40559e
Halogen002 <Halogen002@users.noreply.github.com>
2018-02-27 19:46:29 -0600

    Fix nav points for chimera_fov_fix

--- client/fix/fov_fix.cpp
+++ client/fix/fov_fix.cpp
@@ -4,7 +4,6 @@
 #include "../messaging/messaging.h"
 #include "../halo_data/resolution.h"
 
-static uint32_t resolution_offset = 0;
 static Resolution resolution = {};
 
 static void fix_disparity() noexcept {
@@ -23,13 +22,21 @@ ChimeraCommandError fov_fix_command(size_t argc, const char **argv) noexcept {
         bool new_value = bool_value(argv[0]);
         if(active != new_value) {
             auto &fov_fix_sig = get_signature("fov_fix_sig");
+            auto &fov_disparity_3_sig = get_signature("fov_disparity_3_sig");
             if(new_value) {
                 const unsigned char nop[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
                 write_code_c(fov_fix_sig.address(), nop);
                 add_frame_event(fix_disparity);
+
+                const unsigned char a[] = { 0x66, 0x83, 0xEA, 0x08, 0x90, 0x90, 0x90 };
+                write_code_c(fov_disparity_3_sig.address(), a);
+
+                const unsigned char b[] = { 0x83, 0xE9, 0x08, 0x90, 0x90, 0x90 };
+                write_code_c(fov_disparity_3_sig.address() + 0xD, b);
             }
             else {
                 fov_fix_sig.undo();
+                fov_disparity_3_sig.undo();
                 auto *fov_disparity_1_addr = get_signature("fov_disparity_1_sig").address();
                 auto *fov_disparity_2_addr = get_signature("fov_disparity_2_sig").address();
                 **reinterpret_cast<uint16_t **>(fov_disparity_1_addr + 3) = 8;
