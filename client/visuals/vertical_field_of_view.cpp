a58433bc0fed9c99167d98091e7180e77710f129
Halogen002 <Halogen002@users.noreply.github.com>
2018-03-01 08:47:14 -0600

    Set biped FOV to standard FOV with chimera_vfov

--- client/visuals/vertical_field_of_view.cpp
+++ client/visuals/vertical_field_of_view.cpp
@@ -1,6 +1,8 @@
 #include "vertical_field_of_view.h"
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include "../halo_data/table.h"
+#include "../halo_data/tag_data.h"
 #include "../halo_data/resolution.h"
 #include "../hooks/camera.h"
 #include "../interpolation/camera.h"
@@ -8,12 +10,20 @@
 
 static float vfov = 0.0;
 
+const float standard_vfov = 2.0 * atan(tan(70.0 * M_PI / 180.0 / 2.0) * 3.0 / 4.0) / M_PI * 180.0;
+
 static void on_precamera() {
     static float previous_fov = 0.0;
     auto &data = camera_data();
     if(data.fov == previous_fov) return;
     const float default_fov = 70.00 * M_PI / 180.0;
     auto &resolution = get_resolution();
+
+    char *odata = HaloObject(HaloPlayer().object_id()).object_data();
+    if(odata) {
+        HaloTag tag = HaloTag::from_id(*reinterpret_cast<HaloTagID *>(odata));
+        *reinterpret_cast<float *>(tag.data + 0x1A0) = default_fov;
+    }
 
     float calculated_fov = 2.0 * atan(tan(vfov * M_PI / 180.0 / 2.0) * resolution.width / resolution.height) * (data.fov / default_fov);
     data.fov = calculated_fov;
@@ -27,7 +37,7 @@ ChimeraCommandError vfov_command(size_t argc, const char **argv) noexcept {
             remove_precamera_event(on_precamera);
         }
         else if(new_value == 1) {
-            new_value = 55.41;
+            new_value = standard_vfov;
         }
         else if(new_value < 2 || new_value > 179) {
             console_out_error("FOV must be between 2 and 179");
